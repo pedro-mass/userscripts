ff88f496e6431e8e4f853f334cc008c3ff6cc7a2
Pedro <drom296@gmail.com>
2024-12-30 10:01:04 -0500

    lichess stats: fix not waiting until load issue

--- lichess-stats.js
+++ lichess-stats.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Lichess: Training: Stats for Current Run
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.0.1
 // @description  When doing puzzles, this will show you your stats
 // @copyright    2024, Pedro Mass (https://github.com/pedro-mass)
 // @author       pedro-mass
@@ -9,63 +9,44 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=lichess.org
 // @grant        none
 // @run-at       document-idle
-// ==/UserScript==
 
-(function () {
-  "use strict";
+const selectors = {
+  results: ".result-empty",
+  stats: "#pm-stats",
+  puzzleHolder: ".puzzle__session",
+};
+const constants = {
+  failure: "result-false",
+  success: "result-true",
+};
 
-  const selectors = {
-    results: ".result-empty",
-    stats: "#pm-stats",
-    puzzleHolder: ".puzzle__session",
-  };
-  const constants = {
-    failure: "result-false",
-    success: "result-true",
-  };
+// waitForKeyElements(selectors.results, run);
+wait_element(document, selectors.results).then(run);
 
-  const getResults = () =>
-    Array.from(document.querySelectorAll(selectors.results));
+// helper functions
 
-  const getStatsElem = () =>
-    document.querySelector(selectors.stats) ?? createStatsElem();
+// src: https://stackoverflow.com/a/47406751/2911615
+function wait_element(root, selector) {
+  return new Promise((resolve, reject) => {
+    new MutationObserver(check).observe(root, {
+      childList: true,
+      subtree: true,
+    });
+    function check(changes, observer) {
+      let element = root.querySelector(selector);
+      if (element) {
+        observer.disconnect();
+        resolve(element);
+      }
+    }
+  });
+}
 
-  const createStatsElem = () => {
-    const statsElem = document.createElement("div");
-    statsElem.id = "pm-stats";
-    document.querySelector(selectors.puzzleHolder).appendChild(statsElem);
-
-    return statsElem;
-  };
-
-  const getStats = () => {
-    const results = getResults();
-    const failures = results.filter((x) =>
-      Array.from(x.classList).includes(constants.failure)
-    );
-    const successes = results.filter((x) =>
-      Array.from(x.classList).includes(constants.success)
-    );
-
-    return {
-      total: results.length,
-      failures: failures.length,
-      successes: successes.length,
-    };
-  };
-
-  const displayFailures = (elem) => {
-    const { total, failures } = getStats();
-
-    elem.textContent = `${failures} / ${total} failures`;
-  };
-
-  const displaySuccesses = (elem) => {
-    const { total, successes } = getStats();
-
-    elem.textContent = `${successes} / ${total} successes`;
-  };
-
+function run() {
+  console.log({
+    fn: "run",
+    msg: "starting",
+  });
   const statsElem = getStatsElem();
   displayFailures(statsElem);
   let showFailures = true;
@@ -75,4 +56,48 @@
 
     showFailures = !showFailures;
   });
-})();
+}
+
+function getResults() {
+  return Array.from(document.querySelectorAll(selectors.results));
+}
+
+function getStatsElem() {
+  return document.querySelector(selectors.stats) ?? createStatsElem();
+}
+
+function createStatsElem() {
+  const statsElem = document.createElement("div");
+  statsElem.id = "pm-stats";
+  document.querySelector(selectors.puzzleHolder).appendChild(statsElem);
+
+  return statsElem;
+}
+
+function getStats() {
+  const results = getResults();
+  const failures = results.filter((x) =>
+    Array.from(x.classList).includes(constants.failure)
+  );
+  const successes = results.filter((x) =>
+    Array.from(x.classList).includes(constants.success)
+  );
+
+  return {
+    total: results.length,
+    failures: failures.length,
+    successes: successes.length,
+  };
+}
+
+function displayFailures(elem) {
+  const { total, failures } = getStats();
+
+  elem.textContent = `${failures} / ${total} failures`;
+}
+
+function displaySuccesses(elem) {
+  const { total, successes } = getStats();
+
+  elem.textContent = `${successes} / ${total} successes`;
+}
